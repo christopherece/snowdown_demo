21e9babde17dc488fb027b3a9a6a0e231fc774aa
christopherece <christopheranchetaece@gmail.com>
2024-02-24 14:20:37 +1300

    Added Admin Display

--- incidentdown/admin.py
+++ incidentdown/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import IncidentDown
 
 # Register your models here.
+class IncidentDownAdmin(admin.ModelAdmin):
+    list_display = (
+        'caller_id',
+        'description',
+        'impact',
+        'urgency',
+        'caller_id',
+        'emailAdress',
+        'state',
+        'is_process',
+        'assignment_group',
+        'worknotes',
+        'comments',
+
+    )
+admin.site.register(IncidentDown, IncidentDownAdmin)
+
+
